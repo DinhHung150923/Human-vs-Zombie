ee3a3ee3abbe34410b872dd8de44722b0a236341
DinhHung <ledinhhung150923@gmail.com>
2023-08-28 16:10:19 +0700

    movement

---
+++ Assets/Player/Movement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] protected float speed = 2f;
+    [SerializeField] protected Vector3 direction = Vector3.right;
+    protected virtual void Update()
+    {
+        this.SnakeMovement();
+    }
+    protected virtual void SnakeMovement()
+    {
+        transform.Translate( this.direction*this.speed * Time.deltaTime);
+    }
+}
