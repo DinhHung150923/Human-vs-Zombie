822983c2d45ae7fdb4c1e61db847e62103c30cf3
DinhHung <ledinhhung150923@gmail.com>
2024-02-29 18:53:13 +0700

    .

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Spawner : HungMonobehavior
 {
+    [SerializeField] protected float timer = 0;
+    [SerializeField] protected float timeDelay = 1;
     [SerializeField] protected List<Transform> prefabs;
-    [SerializeField] protected List<Transform> Holders;
+    [SerializeField] protected Transform Obj;
+    [SerializeField] protected Transform Holder;
+    protected int spawnCount = 4;
     protected override void LoadComponent()
     {
         this.LoadPrefab();
+        this.LoadHolder();
     }
     protected override void Awake()
     {
         this.HidePrefab();
+    }
+    protected virtual void FixedUpdate()
+    {
+        this.Spawn();
+    }
+    protected virtual void LoadHolder()
+    {
+        if (this.Holder != null) return;
+        this.Holder = transform.Find("Holder");
+        Debug.LogWarning(transform.name+ "LoadHolder", gameObject);
     }
     protected virtual void LoadPrefab()
     {
@@ -29,5 +44,16 @@ public class Spawner : HungMonobehavior
         {
             prefab.gameObject.SetActive(false);
         }
+    }
+    public virtual void Spawn()
+    {
+        this.timer += Time.deltaTime;
+        if (this.timer < this.timeDelay) return;
+        this.timer = 0;
+
+        Transform newPrefab = Instantiate(this.Obj);
+        this.spawnCount++;
+        newPrefab.gameObject.SetActive(true);
+        newPrefab.SetParent(this.Holder);
     }
 }
